c1edd814b8d43626f158fd05ca26f3c85d076f86
SergioPecan <u20201a938@upc.edu.pe>
2025-06-26 01:02:16 -0500

    git init

---
+++ src/test/java/pe/edu/upc/pc02/Pc02ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.pc02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pc02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
